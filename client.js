e75c8c399e4c1781c9b5eb18aa5b9507d48c96d3
Raynold Ng <raynold.ng24@gmail.com>
2017-04-04 17:08:00 +0800

    fixed eslint errors

--- client.js
+++ client.js
@@ -1,113 +1,105 @@
-const DB_NAME = 'userCursors';
-
-colors = ['BurlyWood', 'PowderBlue', 'Violet', 'GreenYellow', 
+const colors = ['BurlyWood', 'PowderBlue', 'Violet', 'GreenYellow',
   'Red', 'LimeGreen', 'DarkViolet', 'GhostWhite', 'OrangeRed', 'HotPink'];
-MAX_COLORS = 10;
+const MAX_COLORS = 10;
 
 function SharedbMultipleCursors(socket, ace) {
   const MAX_USERS = 50;
 
-  // no error checking for now
-  //const _mode = prompt('Insert (unique) username:', 'user' 
-  //  + Math.floor(Math.random() * MAX_USERS));
-  marker = {};
+  const marker = {};
   marker.cursors = {}; // username -> {color, cursors}
 
-  const username = 'user' + Math.floor(Math.random() * MAX_USERS);
-  console.log('local username: ' + username);
+  const username = `user${Math.floor(Math.random() * MAX_USERS)}`;
 
   // Initialize User
-  socket.addEventListener('open', function() {
-    message = {
-      id:'initUser',
-      userName:username,
-      cursor:ace.getCursorPosition(),
-    }; 
-    socket.send(JSON.stringify(message)); 
+  socket.addEventListener('open', () => {
+    const message = {
+      id: 'initUser',
+      userName: username,
+      cursor: ace.getCursorPosition(),
+    };
+    socket.send(JSON.stringify(message));
   });
 
   // listen for local cursor changes
-  ace.selection.on('changeCursor', function() {
-    console.log('cursor change to: ' + JSON.stringify(ace.getCursorPosition()));
-    message = {
-      id:'cursorChange',
-      userName : username,
-      cursor : ace.getCursorPosition(),
+  ace.selection.on('changeCursor', () => {
+    const message = {
+      id: 'cursorChange',
+      userName: username,
+      cursor: ace.getCursorPosition(),
     };
-    
+
     socket.send(JSON.stringify(message));
-    console.log('sent cursor change to server');
   });
 
-  socket.addEventListener('message', (message) => {
-    console.log('client received: ' + JSON.stringify(message.data));
+  socket.addEventListener('message', (msg) => {
+    let message = null;
     try {
-      message = JSON.parse(message.data);
-    } catch(e) {}
+      message = JSON.parse(msg.data);
+    } catch (e) {
+      return;
+    }
 
-    console.log(message);
     switch (message.id) {
       case 'cursorChange':
-      // if own cursor then ignore
-      console.log('updating cursor data');
-      if (message.userName === username) break;
-      
-      if(!(message.userName in marker.cursors)) {
-        marker.cursors[message.userName] = {
-          cursor : message.cursor,
-          color : colors[Math.floor(Math.random() * MAX_COLORS)]
+        if (message.userName === username) break;
+
+        if (!(message.userName in marker.cursors)) {
+          marker.cursors[message.userName] = {
+            cursor: message.cursor,
+            color: colors[Math.floor(Math.random() * MAX_COLORS)],
+          };
+        } else {
+          marker.cursors[message.userName].cursor = message.cursor;
         }
-      } else {
-        marker.cursors[message.userName].cursor = message.cursor;
-      }
-      //marker.cursors[message.userName] = {cursor : message.cursor, color : 'Green'};
-      console.log(JSON.stringify(marker.cursors));
-      console.log('drawing cursors');
-      marker.redraw();
-      break;
+        marker.redraw();
+        break;
       case 'removeUser':
-        if(message.userName in marker.cursors) {
+        if (message.userName in marker.cursors) {
           delete marker.cursors[message.userName];
         }
-    }
-    });
-
-  marker.update = function(html, markerLayer, session, config) {
-    const start = config.firstRow, end = config.lastRow;
-    const cursors = this.cursors;
-    
-    for(const property in cursors) {
-      //for (var i = 0; i < cursors.length; i++) {
-      var pos = this.cursors[property].cursor;
-      if (pos.row < start) {
-        continue;
-      } else if (pos.row > end) {
         break;
-      } else {
-        // compute cursor position on screen
-        // this code is based on ace/layer/marker.js
-        var screenPos = session.documentToScreenPosition(pos);
+      default:
+    }
+  });
 
-        var height = config.lineHeight;
-        var width = config.characterWidth;
-        var top = markerLayer.$getTop(screenPos.row, config);
-        var left = markerLayer.$padding + screenPos.column * width;
-        // can add any html here 
-        html.push(
-          "<div style='",
-          "position: absolute;",
-          "border-left: 2px solid " + this.cursors[property].color + ";", 
-          "height:", height, "px;",
-          "top:", top, "px;",
-          "left:", left, "px; width:", width, "px'></div>"
-        );
+  marker.update = function (html, markerLayer, session, config) {
+    const start = config.firstRow;
+    const end = config.lastRow;
+    const cursors = this.cursors;
+
+    for (const property in cursors) {
+      // for (var i = 0; i < cursors.length; i++) {
+      if ('cursor' in this.cursors[property]) {
+        const pos = this.cursors[property].cursor;
+        if (pos.row > end) {
+          break;
+        }
+        if (pos.row >= start) {
+          // compute cursor position on screen
+          // this code is based on ace/layer/marker.js
+          const screenPos = session.documentToScreenPosition(pos);
+
+          const height = config.lineHeight;
+          const width = config.characterWidth;
+          const top = markerLayer.$getTop(screenPos.row, config);
+          const left = markerLayer.$padding + (screenPos.column * width);
+          // can add any html here
+          html.push(
+            "<div style='",
+            'position: absolute;',
+            `border-left: 2px solid ${this.cursors[property].color};`,
+            'height:', height, 'px;',
+            'top:', top, 'px;',
+            'left:', left, 'px; width:', width, "px'></div>",
+          );
+        }
       }
     }
-  }
-  marker.redraw = function() {
-    this.session._signal("changeFrontMarker");
   };
-  
+  marker.redraw = function () {
+    this.session._signal('changeFrontMarker');
+  };
+
   marker.session = ace.session;
   marker.session.addDynamicMarker(marker, true);
 }
--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 // import redis from 'redis';
 const redis = require('redis');
+
 const DB_NAME = 'userCursors';
 
 function setDBFactory(redisUrl) {
   return (userName, cursor) => {
-    const rc = redis.createClient(redisUrl); 
-    rc.hset([DB_NAME, userName, JSON.stringify(cursor)], (err, reply) => {
+    const rc = redis.createClient(redisUrl);
+    rc.hset([DB_NAME, userName, JSON.stringify(cursor)], (err) => {
       if (err) throw err;
       rc.quit();
     });
@@ -14,8 +15,8 @@ function setDBFactory(redisUrl) {
 
 function removeFromDBFactory(redisUrl) {
   return (userName) => {
-    const rc = redis.createClient(redisUrl); 
-    rc.del([DB_NAME, userName], (err, reply) => {
+    const rc = redis.createClient(redisUrl);
+    rc.del([DB_NAME, userName], (err) => {
       if (err) throw err;
       rc.quit();
     });
@@ -26,63 +27,62 @@ module.exports = function subscribe(redisUrl) {
   return (ctx) => {
     const setDB = setDBFactory(redisUrl);
     const removeFromDB = removeFromDBFactory(redisUrl);
-    
+
     const pub = redis.createClient(redisUrl);
     const sub = redis.createClient(redisUrl);
 
     sub.on('message', (channel, message) => {
-      if(channel === 'multiple-cursors') {
-        ctx.websocket.send(message); 
+      if (channel === 'multiple-cursors') {
+        ctx.websocket.send(message);
       }
     });
 
     sub.subscribe('multiple-cursors');
 
     // listen to socket messages from client
-    ctx.websocket.on('message', (message) => {
+    ctx.websocket.on('message', (msg) => {
+      let message = null;
       try {
-        message = JSON.parse(message);
-      } catch(e) {
-        console.warn(`unable to parse message: ${message}`);
+        message = JSON.parse(msg);
+      } catch (e) {
+        // console.warn(`unable to parse message: ${message}`);
         return;
       }
 
-      console.log(message);
+      // console.log(message);
+      let pubMessage = null;
+      const userName = message.userName;
+      const cursor = message.cursor;
+      switch (message.id) {
+        case 'initUser': {
+          setDB(userName, cursor);
+          pubMessage = { id: 'newUser', userName };
+          pub.publish('multiple-cursors', JSON.stringify(pubMessage));
+          break;
+        }
+        case 'cursorChange': {
+          setDB(userName, cursor);
 
-      switch(message.id) {
-      case 'initUser':
-        userName = message.userName;
-        cursor = message.cursor;
+          pubMessage = { id: 'cursorChange', userName, cursor };
+          pub.publish('multiple-cursors', JSON.stringify(pubMessage));
+          break;
+        }
+        case 'removeUser': {
+          removeFromDB(userName);
 
-        setDB(userName, cursor);
-        pubMessage = {id : 'newUser', userName : userName};
-        pub.publish('multiple-cursors', JSON.stringify(pubMessage));
-        break;
-      case 'cursorChange':
-        userName = message.userName;
-        cursor = message.cursor;
-        setDB(userName, cursor);
-        
-        pubMessage = {id: 'cursorChange', userName : userName, cursor : cursor};
-        pub.publish('multiple-cursors', JSON.stringify(pubMessage));
-        break;
-      case 'removeUser':
-        userName = message.userName;
-        removeFromDB(userName);
-
-        pubMessage = {id: 'removeUser', userName : userName};
-        pub.publish('multiple-cursors', JSON.stringify(pubMessage));
-        break;
-      default:
-        console.warn(`unhandled message: ${message}`);
+          pubMessage = { id: 'removeUser', userName };
+          pub.publish('multiple-cursors', JSON.stringify(pubMessage));
+          break;
+        }
+        default:
+          // console.warn(`unhandled message: ${message}`);
       }
     });
-    
-    ctx.websocket.on('close', (event) => {
+
+    ctx.websocket.on('close', () => {
       sub.unsubscribe();
       sub.quit();
       pub.quit();
     });
   };
-  
 };
